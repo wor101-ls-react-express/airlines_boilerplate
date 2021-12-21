84bc38a506a39a79dea92d698776cafefcb677e4
Will <wor101@gmail.com>
2021-12-20 19:00:15 -0500

    Trying to fix heroku deployment in index.html

--- src/components/Svg.js
+++ src/components/Svg.js
@@ -7,7 +7,7 @@ const Svg = ({ routes, airports }) => {
     <g transform="scale(1 -1)">
       <image xlinkHref="equirectangular_world.jpg" href="equirectangular_world.jpg" x="-180" y="-90" height="100%" width="100%" transform="scale(1 -1)"/>
       
-      { routes.map(route => {
+      { routes.map((route, index) => {
         const srcAirport = airports.find(airport => airport.code === route.src)
         const x1 = srcAirport.long
         const y1 = srcAirport.lat
@@ -18,7 +18,7 @@ const Svg = ({ routes, airports }) => {
         
 
           return (
-            <g key="">
+            <g key={index}>
               <circle className="source" cx={x1} cy={y1}>
                 <title></title>
               </circle> 
